e087fa1cfbcda14f8f3225c9b7f0dc4928d2f69a
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-09-26 08:52:33 -0500

    Add lalrpop building

---
+++ tests/grammar.rs
@@ -0,0 +1,24 @@
+
+extern crate gears;
+use gears::parser;
+
+const FULL_GRAMMAR_EXAMPLE: &str = r#"
+def simple_math(a, b) {
+    let a = 4 - 3;
+    let b = a * 9;
+    12 * (a + b)
+}
+"#;
+
+#[test]
+fn test_grammar() {
+    let result = parser::ModuleParser::new().parse(FULL_GRAMMAR_EXAMPLE);
+
+    match result {
+        Err(e) => {
+            println!("{:#?}", e);
+            assert!(false, "Grammar Error. Please review output.")
+        }
+        _ => {}
+    }
+}
