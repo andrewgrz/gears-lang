8cab189a7eebb928481c7500d806b2e4b05ff4a6
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-10-01 07:40:15 -0500

    Add an initial compiler file

---
+++ src/compiler.rs
@@ -0,0 +1,22 @@
+
+use std::fs::File;
+use std::io::prelude::*;
+use module::Module;
+use errors::GearsError;
+use parser;
+
+/// Compile a gears file to a module
+pub fn compile_file(filename: &str) -> Result<Module, GearsError> {
+    let mut f = File::open(filename)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+    compile_str(&contents, filename)
+}
+
+/// Compile a String to
+pub fn compile_str(string: &str, name: &str) -> Result<Module, GearsError> {
+    let module = Module::new(String::from(name));
+    let module_ast = parser::ModuleParser::new().parse(string);
+
+    Ok(module)
+}
