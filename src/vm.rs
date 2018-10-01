36432b99ba8b90bcc393cb2180e5c1fe1fab7949
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-10-01 07:45:42 -0500

    Add an initial empty vm impl

---
+++ src/vm.rs
@@ -0,0 +1,8 @@
+
+use module::Module;
+use object::GearsObject;
+use errors::GearsError;
+
+pub fn execute_module(module: Module) -> Result<GearsObject, GearsError> {
+    panic!("Executing Module is not implemented yet!")
+}
