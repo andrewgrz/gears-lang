c085ada7eb3858a8c784704b156d14ed98cc336d
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-10-01 07:41:19 -0500

    Add an initial module file

---
+++ src/module.rs
@@ -0,0 +1,21 @@
+
+use opcodes::Opcodes;
+use std::collections::HashMap;
+
+/// Contains a compiled module
+pub struct Module {
+    name: String,
+    functions: HashMap<String, Function>,
+}
+
+impl Module {
+    pub fn new(name: String) -> Module {
+        Module { name: name, functions: HashMap::new() }
+    }
+}
+
+/// A compiled function
+pub struct Function {
+    name: String,
+    opcodes: Opcodes
+}
