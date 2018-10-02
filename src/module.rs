e4bf396724fb99024126b50c9ba8c5fbcc81785e
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-10-02 08:16:06 -0500

    Add get_function to Module

--- src/module.rs
+++ src/module.rs
@@ -1,5 +1,6 @@
 use opcodes::Opcodes;
 use std::collections::HashMap;
+use errors::GearsError;
 
 /// Contains a compiled module
 pub struct Module {
@@ -14,10 +15,23 @@ impl Module {
             functions: HashMap::new(),
         }
     }
+
+    pub fn get_function(&self, name: &str) -> Result<&Function, GearsError> {
+        match self.functions.get(&name.to_string()) {
+            Some(v) => Ok(v),
+            None => Err(GearsError::FunctionNotFound(name.to_string()))
+        }
+    }
 }
 
 /// A compiled function
 pub struct Function {
     name: String,
     opcodes: Opcodes,
+}
+
+impl Function {
+    pub fn get_opcodes(&self) -> &Opcodes {
+        &self.opcodes
+    }
 }
