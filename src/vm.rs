444de127d853d6653938689e016aba4ecbdf02d7
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-10-03 08:57:13 -0500

    Add test for vm doing addition

--- src/vm.rs
+++ src/vm.rs
@@ -5,7 +5,7 @@ use errors::GearsError;
 use opcodes::*;
 
 /// Execute a function contained in a compiled module
-pub fn execute_function(module: Module, function: &str) -> GearsResult {
+pub fn execute_function(module: &Module, function: &str) -> GearsResult {
     let mod_fn = module.get_function(function)?;
 
     execute(&mod_fn, &module)
@@ -40,15 +40,48 @@ fn execute(function: &Function, module: &Module) -> GearsResult {
         }};
     }
 
+    macro_rules! advance {
+        () => {{
+            cur_instr = opcodes[ip];
+            ip += 1;
+        }};
+    }
+
     loop {
-        cur_instr = opcodes[ip];
-        ip += 1;
+        advance!();
 
         match cur_instr {
             RETURN => return Ok(pop!()),
             BIN_ADD => bin_op!(add),
             BIN_SUB => bin_op!(sub),
+            LOAD_CONST => {
+                advance!();
+                push!((*module.get_const(cur_instr as usize)).clone());
+            },
             _ => return Err(GearsError::InternalCompilerError(format!("Unexpected Opcode: {:?}", cur_instr))),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use module::{ModuleBuilder};
+
+    #[test]
+    fn test_addition() {
+        let mut module_builder = ModuleBuilder::new("Test".to_string());
+
+        module_builder.start_function("simple_math".to_string());
+        module_builder.load_int(3);
+        module_builder.load_int(4);
+        module_builder.op_add();
+        module_builder.load_int(8);
+        module_builder.op_add();
+        module_builder.finish_function();
+
+        let module = module_builder.build();
+        let result = execute_function(&module, "simple_math");
+        assert_eq!(result, Ok(GearsObject::Int(15)));
     }
 }
