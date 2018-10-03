e271499f5a8dd214969271527b8c069c77866fd1
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-10-03 18:00:49 -0500

    Add paritialEq to gears error
    std::io::error does not impl it so we can't derive it

--- src/errors.rs
+++ src/errors.rs
@@ -13,3 +13,31 @@ impl From<io::Error> for GearsError {
         GearsError::RustIOError(error)
     }
 }
+
+impl PartialEq for GearsError {
+    fn eq(&self, other: &GearsError) -> bool {
+        use self::GearsError::*;
+
+        match self {
+            FunctionNotFound(l) => {
+                match other {
+                    FunctionNotFound(r) => l == r,
+                    _ => false
+                }
+            },
+            InternalCompilerError(l) => {
+                match other {
+                    InternalCompilerError(r) => l == r,
+                    _ => false
+                }
+            },
+            TypeError(l) => {
+                match other {
+                    TypeError(r) => l == r,
+                    _ => false
+                }
+            }
+            _ => false
+        }
+    }
+}
