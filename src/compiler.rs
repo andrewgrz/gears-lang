7f442e80525e584bfc10d9cf4d41cad273ee63ae
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-10-07 16:37:05 -0500

    Add compilier code for assignments

--- src/compiler.rs
+++ src/compiler.rs
@@ -4,7 +4,7 @@ use module::{Module, ModuleBuilder};
 use parser;
 use std::fs::File;
 use std::io::prelude::*;
-use symbol::SymbolTable;
+use symbol::{SymbolTable, SymbolType};
 
 /// Compile a gears file to a module
 pub fn compile_file(filename: &str) -> Result<Module, GearsError> {
@@ -38,9 +38,12 @@ fn compile_ast(ast: Vec<Box<ModStmtAst>>, name: &str) -> Result<Module, GearsErr
         match mod_stmt.as_ref() {
             ModStmtAst::FunctionDef { name, exprs, .. } => {
                 module_builder.start_function(name.clone());
+                let mut local_scope = (&symbol_table).push();
+
                 for stmt in exprs {
-                    visit_stmt(stmt.as_ref(), &mut module_builder);
+                    visit_stmt(stmt.as_ref(), &mut local_scope, &mut module_builder)?;
                 }
+
                 module_builder.finish_function();
             }
         }
@@ -49,19 +52,25 @@ fn compile_ast(ast: Vec<Box<ModStmtAst>>, name: &str) -> Result<Module, GearsErr
     Ok(module_builder.build())
 }
 
-fn visit_stmt(stmt: &StmtAst, module_builder: &mut ModuleBuilder) {
+fn visit_stmt(stmt: &StmtAst, scope: &mut SymbolTable, mut module_builder: &mut ModuleBuilder) -> Result<(), GearsError> {
     match stmt {
-        StmtAst::Expr(e) => visit_expr(e, module_builder),
-        StmtAst::Assignment{ .. } => { } // TODO: Write me
+        StmtAst::Expr(e) => visit_expr(e, scope, module_builder)?,
+        StmtAst::Assignment{ name, expr } => { 
+            visit_expr(expr, scope, &mut module_builder)?;
+            let index = scope.def_variable(name.clone());
+            module_builder.store_fast(index);
+        }
     }
+
+    Ok(())
 } 
 
-fn visit_expr(expr: &ExprAst, mut module_builder: &mut ModuleBuilder) {
+fn visit_expr(expr: &ExprAst, scope: &mut SymbolTable, mut module_builder: &mut ModuleBuilder) -> Result<(), GearsError>  {
     match expr {
         ExprAst::Integer(e) => module_builder.load_int(*e),
         ExprAst::Op(left, op, right) => {
-            visit_expr(left, &mut module_builder);
-            visit_expr(right, &mut module_builder);
+            visit_expr(left, scope, &mut module_builder)?;
+            visit_expr(right, scope, &mut module_builder)?;
 
             match op {
                 BinOpAst::Add => module_builder.op_add(),
@@ -70,6 +79,24 @@ fn visit_expr(expr: &ExprAst, mut module_builder: &mut ModuleBuilder) {
                 BinOpAst::Div => module_builder.op_div(),
             }
         }
-        ExprAst::Variable(_) => {} // TODO: write me
+        ExprAst::Variable(name) => {
+            let (maybe_symbol, is_global) = scope.resolve(name);
+            
+            match maybe_symbol {
+                Some(symbol) => {
+                    match symbol.get_type() {
+                        &SymbolType::Function => return Err(GearsError::InternalCompilerError("Functions are callable yet".to_string())),
+                        &SymbolType::Variable => {
+                            if !is_global {
+                                module_builder.load_fast(*symbol.get_index());
+                            }
+                        }
+                    }
+                },
+                None => return Err(GearsError::SymbolNotFound(name.clone())),
+            }
+        } 
     }
+
+    Ok(())
 } 
