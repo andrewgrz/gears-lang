26ec8a18de941da769d255c99d7ae696820defbb
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-10-10 18:12:57 -0500

    Add test for too few or too many args

--- tests/test_function.rs
+++ tests/test_function.rs
@@ -1,8 +1,11 @@
 extern crate gears_lang;
-#[macro_use] extern crate cached;
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate cached;
+#[macro_use]
+extern crate lazy_static;
 
 use gears_lang::compiler::compile_file;
+use gears_lang::errors::*;
 use gears_lang::module::Module;
 use gears_lang::object::GearsObject;
 use gears_lang::vm::execute_function;
@@ -40,4 +43,40 @@ fn pass_args() {
         ).unwrap(),
         GearsObject::Int(92)
     );
+}
+
+#[test]
+fn pass_too_few_args() {
+    match execute_function(&setup(), "main_args", vec![GearsObject::Int(1)]) {
+        Ok(_) => panic!("Should not have passed"),
+        Err(error) => match error {
+            GearsError::InterOpError { error, .. } => match error {
+                InterOpErrorType::TooFewArgs => {}
+                _ => panic!("Wrong error returned"),
+            },
+            _ => panic!("Wrong error returned"),
+        },
+    }
+}
+
+#[test]
+fn pass_too_many_args() {
+    match execute_function(
+        &setup(),
+        "main_args",
+        vec![
+            GearsObject::Int(1),
+            GearsObject::Int(1),
+            GearsObject::Int(1),
+        ],
+    ) {
+        Ok(_) => panic!("Should not have passed"),
+        Err(error) => match error {
+            GearsError::InterOpError { error, .. } => match error {
+                InterOpErrorType::TooManyArgs => {}
+                _ => panic!("Wrong error returned"),
+            },
+            _ => panic!("Wrong error returned"),
+        },
+    }
 }
