2e63d69012f0e80b5e7e926f465a65d78fde56d9
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-11-01 08:55:42 -0500

    Run cargo fmt

--- tests/test_compare.rs
+++ tests/test_compare.rs
@@ -19,12 +19,12 @@ cached!{
 #[test]
 fn test_compare() {
     let test_set = vec![
-        // a, b, <,   <=,   >,    >=,     ==,    != 
+        // a, b, <,   <=,   >,    >=,     ==,    !=
         (3, 4, true, true, false, false, false, true),
         (4, 4, false, true, false, true, true, false),
         (5, 4, false, false, true, true, false, true),
     ];
-    
+
     for row in test_set {
         let a = GearsObject::from(row.0);
         let b = GearsObject::from(row.1);
@@ -32,38 +32,49 @@ fn test_compare() {
         assert_eq!(
             execute_function(&setup(), "compare_less", vec![a.clone(), b.clone()]).unwrap(),
             GearsObject::Bool(row.2),
-            "Compare Failed for: {} < {}", row.0, row.1
+            "Compare Failed for: {} < {}",
+            row.0,
+            row.1
         );
 
         assert_eq!(
             execute_function(&setup(), "compare_less_eq", vec![a.clone(), b.clone()]).unwrap(),
             GearsObject::Bool(row.3),
-            "Compare Failed for: {} <= {}", row.0, row.1
+            "Compare Failed for: {} <= {}",
+            row.0,
+            row.1
         );
 
         assert_eq!(
             execute_function(&setup(), "compare_greater", vec![a.clone(), b.clone()]).unwrap(),
             GearsObject::Bool(row.4),
-            "Compare Failed for: {} > {}", row.0, row.1
+            "Compare Failed for: {} > {}",
+            row.0,
+            row.1
         );
 
         assert_eq!(
             execute_function(&setup(), "compare_greater_eq", vec![a.clone(), b.clone()]).unwrap(),
             GearsObject::Bool(row.5),
-            "Compare Failed for: {} >= {}", row.0, row.1
+            "Compare Failed for: {} >= {}",
+            row.0,
+            row.1
         );
 
         assert_eq!(
             execute_function(&setup(), "compare_eq", vec![a.clone(), b.clone()]).unwrap(),
             GearsObject::Bool(row.6),
-            "Compare Failed for: {} == {}", row.0, row.1
+            "Compare Failed for: {} == {}",
+            row.0,
+            row.1
         );
 
         assert_eq!(
             execute_function(&setup(), "compare_not_eq", vec![a.clone(), b.clone()]).unwrap(),
             GearsObject::Bool(row.7),
-            "Compare Failed for: {} != {}", row.0, row.1
+            "Compare Failed for: {} != {}",
+            row.0,
+            row.1
         );
     }
 }
-
