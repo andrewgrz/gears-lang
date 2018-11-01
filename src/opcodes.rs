57e15d1778e47739f7d69d4f03bcd429937019a6
Andrew Grzeszczak <andrew.s.grzeszczak@gmail.com>
2018-11-01 08:23:49 -0500

    Refactor opcodes to a macro

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,23 +1,48 @@
 pub type Opcodes = Vec<u8>;
 
-// Control Flow Opcodes
-pub const RETURN: u8 = 0;
-pub const CALL_FUNCTION: u8 = 1;
-pub const JUMP: u8 = 2;
-pub const JUMP_IF_FALSE: u8 = 3;
+macro_rules! gen_opcodes {
+    ( $( $code:ident: $index:expr ),* ) => {
+        $(
+            pub const $code: u8 = $index;
+        )*
+        /// Prints out the oppcode for debugging purposes
+        pub fn print_code(code: u8) -> &'static str {
+            match code {
+                $(
+                    $code => stringify!($code),
+                )*
+                _ => "UNKNOWN"
+            }
+        }
+    }
+}
 
-// Binary Opcodes
-pub const BIN_ADD: u8 = 4;
-pub const BIN_SUB: u8 = 5;
-pub const BIN_MUL: u8 = 6;
-pub const BIN_DIV: u8 = 7;
+gen_opcodes!(
+    RETURN: 0,
+    CALL_FUNCTION: 1,
+    JUMP: 2,
+    JUMP_ABSOLUTE: 3,
+    JUMP_IF_FALSE: 4,
 
-// Misc Opcodes
-pub const LOAD_CONST: u8 = 20;
+    // Binary Opcodes
+    BIN_ADD: 10,
+    BIN_SUB: 11,
+    BIN_MUL: 12,
+    BIN_DIV: 13,
+    BIN_EQUAL: 14,
+    BIN_NOT_EQUAL: 15,
+    BIN_LESS_THAN: 16,
+    BIN_LESS_THAN_EQUAL: 17,
+    BIN_GREATER_THAN: 18,
+    BIN_GREATER_THAN_EQUAL: 19,
 
-// Loading and Storing
-pub const LOAD_FAST: u8 = 30;
-pub const STORE_FAST: u8 = 31;
-pub const LOAD_TRUE: u8 = 32;
-pub const LOAD_FALSE: u8 = 33;
-pub const LOAD_NONE: u8 = 34;
+    // Misc Opcodes
+    LOAD_CONST: 20,
+
+    // Loading and Storing
+    LOAD_FAST: 30,
+    STORE_FAST: 31,
+    LOAD_TRUE: 32,
+    LOAD_FALSE: 33,
+    LOAD_NONE: 34
+);
